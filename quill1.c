73555116a68a44e76a99d9fef54e60214563f7cd
malo <tomos4444@outlook.com>
2025-09-30 17:45:06 +0100

    Update quill1.c

--- quill1.c
+++ quill1.c
@@ -20,10 +20,16 @@ void disable_raw_mode() {
 void enable_raw_mode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disable_raw_mode);
-
     struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO | ICANON); 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void print_header() {
+    write(STDOUT_FILENO, "---------------------------------------------------------\n", 58);
+    write(STDOUT_FILENO, " WELCOME TO QUILL TEXT EDITOR - PRESS CTRL C TO BREAK\n", 54);
+    write(STDOUT_FILENO, " QUILL - YOU ARE ON INSERT MODE PRESS ESC TO CHANGE\n", 53);
+    write(STDOUT_FILENO, "---------------------------------------------------------\n", 58);
 }
 
 void save_file(const char* filename) {
@@ -41,6 +47,11 @@ void handle_command() {
     if (strncmp(command_buffer, ":quill save ", 12) == 0) {
         char* filename = command_buffer + 12;
         save_file(filename);
+    } else if (strcmp(command_buffer, ":clear") == 0) {
+        buffer_len = 0;
+        memset(text_buffer, 0, sizeof(text_buffer));
+        write(STDOUT_FILENO, "\033[2J\033[H", 7);
+        print_header();
     } else {
         printf("\nUNKNOWN COMMAND: %s\n", command_buffer);
     }
@@ -51,10 +62,7 @@ void handle_command() {
 
 int main() {
     enable_raw_mode();
-    printf("---------------------------------------------------------\n");
-    printf("WELCOME TO QUILL TEXT EDITOR - PRESS CTRL TO BREAK\n");
-    printf("QUILL - YOU ARE ON INSERT MODE PRESS ESC TO CHANGE\n\n");
-    printf("---------------------------------------------------------");
+    print_header();
 
     while (1) {
         char c;
@@ -63,25 +71,29 @@ int main() {
         if (in_command_mode) {
             if (c == '\n') {
                 handle_command();
-            } else if (c == 127) {  
+            } else if (c == 127) {
                 int len = strlen(command_buffer);
                 if (len > 0) {
                     command_buffer[len - 1] = '\0';
-                    write(STDOUT_FILENO, "\b \b", 3);  
+                    write(STDOUT_FILENO, "\b \b", 3);
                 }
             } else if (strlen(command_buffer) < MAX_COMMAND - 1) {
-                command_buffer[strlen(command_buffer)] = c;
+                int len = strlen(command_buffer);
+                command_buffer[len] = c;
+                command_buffer[len + 1] = '\0';
                 write(STDOUT_FILENO, &c, 1);
             }
         } else {
-            if (c == 27) {  
+            if (c == 27) {
                 in_command_mode = 1;
                 memset(command_buffer, 0, sizeof(command_buffer));
-                printf("\n:");
-            } else if (c == 127) {  
+                command_buffer[0] = ':'; 
+                command_buffer[1] = '\0';
+                write(STDOUT_FILENO, "\n\n---------------------------------------------------------\n:", 61);
+            } else if (c == 127) {
                 if (buffer_len > 0) {
                     buffer_len--;
-                    write(STDOUT_FILENO, "\b \b", 3);  
+                    write(STDOUT_FILENO, "\b \b", 3);
                 }
             } else if (buffer_len < MAX_BUFFER - 1) {
                 text_buffer[buffer_len++] = c;
